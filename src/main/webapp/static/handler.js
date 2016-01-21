422436904ed2ef8dd56e2923677020d09cf94290
Anil Chowdary Maddukuri <anilmaddukuri@gmail.com>
2016-01-21 16:50:16 +0530

    Update handler.js

--- src/main/webapp/static/handler.js
+++ src/main/webapp/static/handler.js
@@ -1,8 +1,38 @@
-var handler = function(event) {
+//Event publisher
+/*var publishSignUpClickEvent = function(event) {
+	console.log($('#signUp').attr('data-custom-event'));
+	var modifiedEvent = $('#signUp').attr('data-custom-event');
+	$('#signUp').trigger(modifiedEvent);
+	event.preventDefault();
+};*/
+
+//Event handler
+var signInHandler = function(event) {
 	console.log(event.type);
 	console.log("clicked");
-	event.preventDefault();
 };
+
+var indexSignUpHandler = function(event) {
+	$.ajax({
+		url: "signUp.html",
+		type: "GET",
+		success: function(json) {
+			$("#formDiv").replaceWith(json);
+		},
+		error: function(xhr, status, errorThrown) {
+			console.log("status: " + status);
+			console.log("error thrown: " + errorThrown);
+			console.dir(xhr);
+		}
+	});
+};
+
+var actualsignUpHandler = function(event) {
+	console.log("Make a call to server to register the user");
+	return true;
+};
+
 $(function() {
-	$('.form-signin').on('click', handler);
+	$('#signUp').on('click', indexSignUpHandler);
+	$('#signIn').on('click', signInHandler);
 });
